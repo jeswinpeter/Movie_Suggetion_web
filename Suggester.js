f107f3d64d03b695dcb774f876f4a563f28df33c
jeswin <jeswinpeter92@gmail.com>
2025-12-16 22:41:57 +0530

    Unknown modification, pushing anyway

--- Suggester.js
+++ Suggester.js
@@ -9,19 +9,35 @@ inputBox.addEventListener("keydown", function(event) {
 
         if(movieName === "") return;
 
-        let apiUrl = `https://api.themoviedb.org/3/search/movie?api_key='Place Your api key here'&query=${encodeURIComponent(movieName)}`;
+        let apiUrl = `https://api.themoviedb.org/3/search/movie?api_key=ed507b779fc1ae8e24b7f99587efec8b&query=${encodeURIComponent(movieName)}`;
 
         fetch(apiUrl)
             .then(response => response.json())
             .then(data => {
                 if(data.results.length > 0) {
                     let movie = data.results[0];
-                    resultText.innerText = `Found: ${movie.title}`;
+                    let detailsBox = document.getElementById("movieDetails");
+
+                    document.querySelector(".bubble").classList.add("expanded");
+
+                    detailsBox.innerHTML = `
+                    <p><strong>Title:</strong> ${movie.title}</p>
+                    <p><strong>Release date:</strong> ${movie.release_date}</p>
+                    <p><strong>Rating:</strong> ${movie.vote_average}</p>
+                    <p><strong>Summary:</strong> ${movie.overview}</p>
+                    `;
+
+                    detailsBox.style.display = "block";
+                    // detailsBox.scrollIntoView({behavior: "smooth"})
                 }
                 else {
                     resultText.innerText = `No Movie found for "${movieName}"`;
                 }
             })
+        .catch(error => {
+            console.error("Error fetching movie:", error);
+            resultText.innerText = "Something went wrong. Try again."
+        })
     }
 });
 
